79eb794129f9c8384c778087ab89c5e5263f7d5b
Vanya U <ivict@rambler.ru>
2022-09-13 19:48:44 +0300

    # IQ-1 // Initial implementation
      --  There is the fix for the version.

--- infiq/__init__.py
+++ infiq/__init__.py
@@ -7,3 +7,27 @@
 # Producer
 
 # Payload
+
+VERSION = (0, 1, 1, 'alpha')
+
+
+def get_release():
+    return '-'.join([get_version(), VERSION[-1]])
+
+
+def get_version():
+    """
+    Returns only digit parts of version.
+    """
+    return '.'.join(str(i) for i in VERSION[:3])
+
+
+__author__ = 'usalko'
+__docformat__ = 'restructuredtext en'
+__copyright__ = 'Copyright 2022, usalko'
+__license__ = 'Apache Software License'
+__version__ = get_version()
+__maintainer__ = 'usalko'
+__email__ = 'ivict@rambler.ru'
+__status__ = 'Development'
+
--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ from setuptools import setup, find_packages
 from infiq import get_version as get_package_version
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
-REQUIREMENTS = os.path.join(os.path.dirname(__file__), 'requirements.txt')
-reqs = open(REQUIREMENTS).read().splitlines()
+REQUIREMENTS = open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).read().splitlines()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -15,7 +14,9 @@ setup(
     version=get_package_version(),
     packages=find_packages(),
     include_package_data=True,
-    install_requires=reqs,
+    install_requires=REQUIREMENTS,
+    setup_requires=REQUIREMENTS,
+    pyrobuf_modules='proto',
     license='APACHE 2.0',
     description='Some widgets collections fro the Django 4.0+',
     long_description=README,
